2bc5f6be6983bb039a34971532895c6e3321c107
henriqueaguiiar <henriqueaguiarpacheco09@gmail.com>
2025-10-07 21:18:11 -0300

    initial commit

---
+++ src/main/java/com/henriqueaguiiar/todo_api_mvc/TodoApiMvcApplication.java
@@ -0,0 +1,13 @@
+package com.henriqueaguiiar.todo_api_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henriqueaguiiar/todo_api_mvc/TodoApiMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriqueaguiiar.todo_api_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
